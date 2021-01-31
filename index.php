5422f06d05cf55f18479246b753e197bc0222d0a
WahyutriNugroho <wahyutrinugroho594@gmail.com>
2021-01-31 21:22:44 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 <body>
     <h1>hello world</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A quos architecto itaque quia, possimus, quo explicabo ut quasi odit perspiciatis exercitationem repellendus harum officia autem praesentium asperiores minima. Vero, quos?</p>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+    </ul>
 </body>
 
 </html>
